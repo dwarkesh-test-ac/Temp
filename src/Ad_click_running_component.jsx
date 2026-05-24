b5a7fb24c5a8db5098ce7ee74fa60149a0ccfa86
dwarkesh-test-ac <otp_by_travel1@proton.me>
2026-05-24 17:16:44 +0000

    new files add

---
+++ src/Ad_click_running_component.jsx
@@ -0,0 +1,6 @@
+import React from 'react'
+export default function Ad_click_runing_component() {
+  return (
+    <div>Ad_click_runing_component</div>
+  )
+}
---
+++ src/index.js
@@ -0,0 +1,4 @@
+import AdRunner from "./Ad_auto_runing_component";
+import AdClick from "./Ad_click_running_component";
+
+export { AdRunner, AdClick};

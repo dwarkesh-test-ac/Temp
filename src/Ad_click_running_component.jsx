d9ae3fcac5eac25d81d9ef5732ffdb7be9a5aa7c
dwarkesh-test-ac <otp_by_travel1@proton.me>
2026-05-26 23:09:20 +0530

    Delete src directory

--- src/Ad_click_running_component.jsx
+++
@@ -1,6 +0,0 @@
-import React from 'react'
-export default function ClickAdRunner() {
-  return (
-    <div>Ad_click_runing_component</div>
-  )
-}
--- src/index.jsx
+++
@@ -1,5 +0,0 @@
-import AdRunner from "./Ad_auto_runing_component";
-import AdClick from "./Ad_click_running_component";
-
-export default AdRunner;
-export { AdClick};
